5b802a57bd0575137a12ec83b58dc02612e0a001
Charles <schneidercw1@gmail.com>
2022-08-30 14:48:20 -0400

    changes

--- app/src/main/java/com/schneider/dailywins/network/Resource.kt
+++ app/src/main/java/com/schneider/dailywins/network/Resource.kt
@@ -3,7 +3,9 @@ package com.schneider.dailywins.network
 import okhttp3.ResponseBody
 
 sealed class Resource<out T> {
+
     data class Success<out T>(val value: T) : Resource<T>()
+
     data class Failure(
         val isNetworkError: Boolean,
         val errorCode: Int?,
